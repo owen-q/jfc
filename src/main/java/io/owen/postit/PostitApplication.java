abfc57403b5f82a518564145d9127d2c888a76ac
owen-q <owen.q.dev@gmail.com>
2018-07-09 07:53:14 +0900

    First commit

---
+++ src/main/java/io/owen/postit/PostitApplication.java
@@ -0,0 +1,14 @@
+package io.owen.postit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class PostitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostitApplication.class, args);
+	}
+}
---
+++ src/main/java/io/owen/postit/config/WebConfig.java
@@ -0,0 +1,71 @@
+package io.owen.postit.config;
+
+
+import io.owen.postit.handler.PostHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.thymeleaf.spring5.SpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private PostHandler postHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> index(){
+        return RouterFunctions.route(RequestPredicates.GET("/"), postHandler::index);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> keyboard(){
+        return RouterFunctions.route(RequestPredicates.GET("/keyboard"), postHandler::handleKeyboards);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> message(){
+        return RouterFunctions.route(RequestPredicates.POST("/message"), postHandler::handleMessage);
+    }
+
+    @Bean
+    public SpringWebFluxTemplateEngine templateEngine(){
+        SpringWebFluxTemplateEngine templateEngine = new SpringWebFluxTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("classpath:/templates/");
+
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafReactiveViewResolver thymeleafReactiveViewResolver = new ThymeleafReactiveViewResolver();
+        thymeleafReactiveViewResolver.setTemplateEngine(templateEngine());
+        thymeleafReactiveViewResolver.setApplicationContext(this.applicationContext);
+
+        registry.viewResolver(thymeleafReactiveViewResolver);
+    }
+}
