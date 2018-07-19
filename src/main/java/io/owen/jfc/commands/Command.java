ef99cc50ae9f31c2da6a42b4f81e2bb0c8df6e40
owen-q <owen.q.dev@gmail.com>
2018-07-19 17:42:32 +0900

    Make component

--- src/main/java/io/owen/jfc/commands/Command.java
+++ src/main/java/io/owen/jfc/commands/Command.java
@@ -1,5 +1,7 @@
 package io.owen.jfc.commands;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +13,7 @@ import java.lang.annotation.Target;
 
 @Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
+@Component
 public @interface Command {
     // name
     String name() default "";
