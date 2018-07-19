3fc2675d75332897084018450ca0f9e10b94b659
owen-q <owen.q.dev@gmail.com>
2018-07-19 17:00:28 +0900

    add get/setter

--- src/main/java/io/owen/jfc/model/ButtonsKeyboard.java
+++ src/main/java/io/owen/jfc/model/ButtonsKeyboard.java
@@ -1,8 +1,13 @@
 package io.owen.jfc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by owen_q on 2018. 7. 19..
  */
+@Getter
+@Setter
 public class ButtonsKeyboard implements Keyboard{
     private KeyboardType type = KeyboardType.BUTTONS;
     private String[] buttons;
--- src/main/java/io/owen/jfc/model/TextKeyboard.java
+++ src/main/java/io/owen/jfc/model/TextKeyboard.java
@@ -1,10 +1,18 @@
 package io.owen.jfc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by owen_q on 2018. 7. 19..
  */
 
+@Getter
+@Setter
 public class TextKeyboard implements Keyboard{
     private KeyboardType type = KeyboardType.TEXT;
+
+
+
 
 }
