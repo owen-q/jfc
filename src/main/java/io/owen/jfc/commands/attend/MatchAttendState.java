03ebd83fb5b162d199ffcfe87854755ebcd8053a
owen-q <owen.q.dev@gmail.com>
2018-07-25 08:41:43 +0900

    d

--- src/main/java/io/owen/jfc/commands/attend/MatchAttendState.java
+++ src/main/java/io/owen/jfc/commands/attend/MatchAttendState.java
@@ -3,12 +3,24 @@ package io.owen.jfc.commands.attend;
 import io.owen.jfc.commands.Command;
 import io.owen.jfc.commands.CommandHandler;
 import io.owen.jfc.commands.UserState;
+import io.owen.jfc.common.entity.Match;
+import io.owen.jfc.common.entity.User;
+import io.owen.jfc.common.repository.MatchRepository;
+import io.owen.jfc.common.repository.UserRepository;
+import io.owen.jfc.core.StateList;
+import io.owen.jfc.model.KeyboardType;
 import io.owen.jfc.model.Response;
+import io.owen.jfc.model.ResponseBuilder;
+import io.owen.jfc.util.Cache;
+import io.owen.jfc.util.WeekConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by owen_q on 2018. 7. 20..
@@ -19,6 +31,19 @@ import java.util.Map;
 public class MatchAttendState implements CommandHandler {
     private Logger logger = LoggerFactory.getLogger(MatchAttendState.class);
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private StateList stateList;
+
+    @Autowired
+    private Cache cache;
+
     @Override
     public Response handleInput(String userKey, Map<String, Object> attrs) {
         return null;
@@ -26,7 +51,54 @@ public class MatchAttendState implements CommandHandler {
 
     @Override
     public Response handleCommand(String userKey, Map<String, Object> attrs) {
-        return null;
+        if(logger.isInfoEnabled())
+            logger.info("{} handle", UserState.MATCH_ATTEND.getValue());
+
+        Optional<Match> maybeMatch = (Optional) cache.get(userKey);
+        Response response = null;
+
+        response = maybeMatch
+                .map((match)->{
+                    User attendUser = userRepository.findByUserKey(userKey);
+
+                    match.getAttendList().add(attendUser);
+
+                    Match savedMatch = matchRepository.save(match);
+
+                    /*
+                    // TODO
+                    if(savedMatch.getId() == match.getId())
+                        // success
+                        ;
+                    else
+                        // Fail
+                    ;
+                    */
+
+                    StringBuilder resultMessageBuilder = new StringBuilder();
+
+                    String messageFormat = "> %s (%s) 경기에 %s되었습니다.";
+                    resultMessageBuilder.append(String.format(messageFormat, match.getMatchDate().format(DateTimeFormatter.ISO_DATE), WeekConverter.convert(match.getMatchDate().getDayOfWeek()), UserState.MATCH_ATTEND.getValue()));
+
+                    Response successResponse = new ResponseBuilder()
+                            .keyboardType(KeyboardType.BUTTONS)
+                            .message(resultMessageBuilder.toString())
+                            .buttons(stateList.getMainCommands())
+                            .build();
+
+                    return successResponse;
+                })
+                .orElseGet(()->{
+                    Response failResponse = new ResponseBuilder()
+                            .keyboardType(KeyboardType.BUTTONS)
+                            .buttons(stateList.getMainCommands())
+                            .message("오류가 발생했습니다...")
+                            .build();
+
+                    return failResponse;
+                });
+
+        return response;
     }
 
     @Override
